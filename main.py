35fdbfe8c4ca51f6bb791cc7dca09361048f0f94
baracdavid <91557537+baracdavid@users.noreply.github.com>
2021-09-29 14:13:37 +0300

    am schimbat fisierul

--- main.py
+++ main.py
@@ -3,13 +3,32 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
-  
-  
+  if (n % 2 ==0 and n != 2 and n==1):
+   print("false")
+   break
+  else:
+        d=1
+        for d in range (n)
+        if  (n%d==0):
+            print("false')
+                  break
+        else: print("true")
+is_prime(n)
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
+  p=1
+
+  list []
+  for x in list
+      p*=p*x
+      if p==1
+        retrun 0
+
+
+
   
   
 '''
